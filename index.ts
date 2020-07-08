ad0d4c3d4c4ea93c89d8d8536aace264ca840385
saurabh gupta <saurabhguptarock119@gmail.com>
2020-07-08 16:02:29 +0530

    added common interfaces

--- index.ts
+++ index.ts
@@ -1,3 +1,50 @@
-interface User {}
+interface User {
+  name: string;
+  email: string;
+  uid: string;
+  status: string;
+  coins: number;
+  gender: Gender;
+  addresses: Address[];
 
-export { User };
+  dob?: Date;
+  alternateMobileNo?: string;
+  nickname?: string;
+  photoUrl?: string;
+  phoneNumber?: string;
+}
+
+interface Wishlist {}
+
+interface Cart {
+  email: string;
+  createdAt: Date;
+  lastUpdatedAt: Date;
+}
+
+interface Order {
+  email: string;
+  productName: string;
+  orderId: string;
+  status: Status;
+  createdAt: Date;
+  lastUpdatedAt: Date;
+}
+
+interface Address {
+  name: string;
+  phoneNumber: string;
+  address: string;
+  locality: string;
+  city: string;
+  state: string;
+  pinCode: number;
+  isDefault: boolean;
+  type: TypeOfAddress;
+}
+
+type Status = "shipped" | "delivered";
+type TypeOfAddress = "home" | "office";
+type Gender = "male" | "female";
+
+export { User, Cart, Order, Wishlist };
