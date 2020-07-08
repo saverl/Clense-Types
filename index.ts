6a02aafb3892db90454c0346f1ec7b08d0354712
saurabh gupta <saurabhguptarock119@gmail.com>
2020-07-08 23:25:05 +0530

    added products data model

--- index.ts
+++ index.ts
@@ -14,10 +14,74 @@ interface User {
   phoneNumber?: string;
 }
 
-interface Wishlist {}
+interface Product {
+  id: string;
+  type: ProductType;
+  for: Gender;
+  name: string;
+  description: string;
+  originalPrice: string;
+  offerPrice: string;
+  offerPercent: number;
+  images: string[];
+  details: string;
+  seller: string;
+
+  material?: string;
+  keywords?: string[];
+}
+
+interface ProductType {
+  isUpperBody: boolean;
+  isLowerBody: boolean;
+  isFullBody: boolean;
+
+  sizes: {
+    S?: boolean;
+    M?: boolean;
+    L?: boolean;
+    X?: boolean;
+    XL?: boolean;
+    XXL?: boolean;
+    26?: boolean;
+    28?: boolean;
+    30?: boolean;
+    32?: boolean;
+    34?: boolean;
+    36?: boolean;
+    38?: boolean;
+    40?: boolean;
+    42?: boolean;
+    oneSize?: boolean;
+  };
+  quantity: {
+    S?: number;
+    M?: number;
+    L?: number;
+    X?: number;
+    XL?: number;
+    XXL?: number;
+    26?: number;
+    28?: number;
+    30?: number;
+    32?: number;
+    34?: number;
+    36?: number;
+    38?: number;
+    40?: number;
+    42?: number;
+    oneSize?: number;
+  };
+}
+
+interface Wishlist {
+  email: string;
+  orderId: string;
+}
 
 interface Cart {
   email: string;
+  orderId: string;
   createdAt: Date;
   lastUpdatedAt: Date;
 }
@@ -47,4 +111,4 @@ type Status = "shipped" | "delivered";
 type TypeOfAddress = "home" | "office";
 type Gender = "male" | "female";
 
-export { User, Cart, Order, Wishlist };
+export { User, Cart, Order, Wishlist, Product };
